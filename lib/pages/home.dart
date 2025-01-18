d96f50e38af9a2c44b432eadbb24c991c47b91da
erwil <rwan.legoff@gmail.com>
2025-01-18 23:17:30 +0100

    fractionning into different components the todolist

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fitness/components/todo-list.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,26 +10,13 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text(
-            'Home Page',
+            'To-Do List',
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
           centerTitle: true,
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          leading: Container(
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.blueGrey[50]),
-            child: SvgPicture.asset(
-              'assets/icons/Arrow - Left 2.svg',
-              height: 20,
-              width: 20,
-            ),
-          ),
         ),
         body: const Center(
-          child: Text('Bienvenue sur la page d’accueil !'),
+          child: TodoList(),
         ));
   }
 }
